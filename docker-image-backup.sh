89c4c63ebf2faa58fd58140e63ffa3da96230a86
suren <zxjlwt@126.com>
2018-06-21 12:53:43 +0800

    Update docker-image-backup.sh

--- docker-image-backup.sh
+++ docker-image-backup.sh
@@ -12,6 +12,7 @@ tag=$3
 namespace='surenpi'
 echo $namespace
 
+echo "prepare push to hangzhou"
 docker login registry.cn-hangzhou.aliyuncs.com
 
 cmd="docker tag $srcNamespace/$imageId:$tag registry.cn-hangzhou.aliyuncs.com/$namespace/$imageId:$tag"
@@ -19,5 +20,27 @@ echo "prepare to execute: $cmd"
 $cmd
 
 cmd="docker push registry.cn-hangzhou.aliyuncs.com/$namespace/$imageId:$tag"
+echo "prepare to execute: $cmd"
+$cmd
+
+echo "prepare push to beijing"
+docker login registry.cn-beijing.aliyuncs.com
+
+cmd="docker tag $srcNamespace/$imageId:$tag registry.cn-beijing.aliyuncs.com/$namespace/$imageId:$tag"
+echo "prepare to execute: $cmd"
+$cmd
+
+cmd="docker push registry.cn-beijing.aliyuncs.com/$namespace/$imageId:$tag"
+echo "prepare to execute: $cmd"
+$cmd
+
+echo "prepare push to qingdao"
+docker login registry.cn-qingdao.aliyuncs.com
+
+cmd="docker tag $srcNamespace/$imageId:$tag registry.cn-qingdao.aliyuncs.com/$namespace/$imageId:$tag"
+echo "prepare to execute: $cmd"
+$cmd
+
+cmd="docker push registry.cn-qingdao.aliyuncs.com/$namespace/$imageId:$tag"
 echo "prepare to execute: $cmd"
 $cmd
