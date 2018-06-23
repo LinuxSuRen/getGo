bd9be1ddba19831e442dd7ae5f1817bb08ee0560
xjzhao <xjzhao@alauda.io>
2018-06-23 14:48:22 +0800

    prepare default config file

--- jx-install.sh
+++ jx-install.sh
@@ -3,3 +3,6 @@
 echo "Prepare docker images for Jenkins X"
 
 curl https://raw.githubusercontent.com/LinuxSuRen/getGo/master/jx-images.sh | bash
+
+wget https://raw.githubusercontent.com/LinuxSuRen/getGo/master/.kube/config
+mv config .kube
