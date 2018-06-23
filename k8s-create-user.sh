fa65316b4afc44fc51aa77b26f4b3cb167b5cb10
xjzhao <xjzhao@alauda.io>
2018-06-23 12:53:04 +0800

    let script can exec for many times

--- k8s-create-user.sh
+++ k8s-create-user.sh
@@ -3,6 +3,9 @@ NAME="$1"
 if [ "$1" == "" ]; then
     NAME="user"
 fi
+
+kubectl delete clusterrolebinding ${NAME}-admin
+
 openssl genrsa -out ${NAME}.key 2048
 openssl req -new -key ${NAME}.key -out ${NAME}.csr -subj "/CN=${NAME}/O=jx"
 openssl x509 -req -in ${NAME}.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out ${NAME}.crt -days 800
@@ -10,3 +13,8 @@ openssl x509 -req -in ${NAME}.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kub
 kubectl create clusterrolebinding ${NAME}-admin \
     --clusterrole=cluster-admin \
     --user=${NAME}
+
+wget https://raw.githubusercontent.com/LinuxSuRen/getGo/master/kube-user-install.sh -o install.sh
+
+tar czvf ${NAME}.tar.gz ${NAME}* install.sh
+rm -rfv ${NAME}.key ${NAME}.csr ${NAME}.crt install.sh
