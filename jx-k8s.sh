5c5278fd8a76407385a49062e27b42f6373f5fe2
xjzhao <xjzhao@alauda.io>
2018-06-23 14:33:33 +0800

    clean ake

--- jx-k8s.sh
+++ jx-k8s.sh
@@ -3,4 +3,11 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
-wget http://get.alauda.cn/deploy/ake/ake && chmod u+x ake && ./ake up
+rm -rfv ake
+
+wget http://get.alauda.cn/deploy/ake/ake
+
+chmod u+x ake
+./ake up
+
+rm -rfv ake
