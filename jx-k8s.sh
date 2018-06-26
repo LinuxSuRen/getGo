35feb92b52f052073660f7cf0ba3c743ae14f971
suren <zxjlwt@126.com>
2018-06-26 12:54:44 +0800

    auto install k8s

--- jx-k8s.sh
+++ jx-k8s.sh
@@ -3,11 +3,13 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+curl http://get.alauda.cn/deploy/ake/cleanup | bash
+
 rm -rfv ake
 
 wget http://get.alauda.cn/deploy/ake/ake
 
 chmod u+x ake
-./ake up
+./ake up --cert-sans $1
 
 rm -rfv ake
