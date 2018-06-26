023ce02c1bcde0a79cb43dff095dee8e879abd49
suren <zxjlwt@126.com>
2018-06-26 15:29:44 +0800

    fiexes volume path

--- jx-local-k8s.sh
+++ jx-local-k8s.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-kubectl patch deploy -n jx jenkins --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/3", "value":{"hostPath":{"path": "~.k8s.data/jenkins","type": ""},"name": "jenkins-home"}}]'
+kubectl patch deploy -n jx jenkins --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/3", "value":{"hostPath":{"path": "/root/.k8s.data/jenkins","type": ""},"name": "jenkins-home"}}]'
 kubectl patch deploy -n jx jenkins -p '{"spec":{"template":{"spec":{"nodeSelector":{"name":"master"}}}}}'
 
-kubectl patch deploy -n jx jenkins-x-nexus --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/1", "value":{"hostPath":{"path": "~.k8s.data/jenkins-x-nexus","type": ""},"name": "nexus-data-volume"}}]'
+kubectl patch deploy -n jx jenkins-x-nexus --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/1", "value":{"hostPath":{"path": "/root/.k8s.data/jenkins-x-nexus","type": ""},"name": "nexus-data-volume"}}]'
 kubectl patch deploy -n jx jenkins-x-nexus -p '{"spec":{"template":{"spec":{"nodeSelector":{"name":"master"}}}}}'
 
-kubectl patch deploy -n jx jenkins-x-mongodb --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/1", "value":{"hostPath":{"path": "~.k8s.data/jenkins-x-mongodb","type": ""},"name": "data"}}]'
+kubectl patch deploy -n jx jenkins-x-mongodb --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/1", "value":{"hostPath":{"path": "/root/.k8s.data/jenkins-x-mongodb","type": ""},"name": "data"}}]'
 kubectl patch deploy -n jx jenkins-x-mongodb -p '{"spec":{"template":{"spec":{"nodeSelector":{"name":"master"}}}}}'
 
-kubectl patch deploy -n jx jenkins-x-chartmuseum --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/0", "value":{"hostPath":{"path": "~.k8s.data/jenkins-x-chartmuseum","type": ""},"name": "storage-volume"}}]'
+kubectl patch deploy -n jx jenkins-x-chartmuseum --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/0", "value":{"hostPath":{"path": "/root/.k8s.data/jenkins-x-chartmuseum","type": ""},"name": "storage-volume"}}]'
 kubectl patch deploy -n jx jenkins-x-chartmuseum -p '{"spec":{"template":{"spec":{"nodeSelector":{"name":"master"}}}}}'
 
-kubectl patch deploy -n jx jenkins-x-docker-registry --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/0", "value":{"hostPath":{"path": "~.k8s.data/jenkins-x-docker-registry","type": ""},"name": "data"}}]'
+kubectl patch deploy -n jx jenkins-x-docker-registry --type='json' -p='[{"op": "replace", "path": "/spec/template/spec/volumes/0", "value":{"hostPath":{"path": "/root/.k8s.data/jenkins-x-docker-registry","type": ""},"name": "data"}}]'
 kubectl patch deploy -n jx jenkins-x-docker-registry -p '{"spec":{"template":{"spec":{"nodeSelector":{"name":"master"}}}}}'
 
 kubectl patch deploy -n kube-system jxing-nginx-ingress-controller -p '{"spec":{"template":{"spec":{"hostNetwork":true}}}}'
